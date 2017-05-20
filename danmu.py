d14ca0d5a1e19499e562e95f190ff09b6814779b
Meng Yang <671840078@qq.com>
2017-05-19 23:35:11 -0700

    add title

--- danmu.py
+++ danmu.py
@@ -12,14 +12,18 @@ head = {
 }
 
 
-def get_comments(av):
+def get_comments(av, index=None):
     # f = open(av + '.txt', 'w')
-    url = 'http://bilibili.com/video/av' + str(av)
+    if index:
+        url = 'http://bilibili.com/video/av{}/index_{}.html'.format(str(av), index)
+    else:
+        url = 'http://bilibili.com/video/av' + str(av)
     html = requests.get(url, headers=head)
     selector = etree.HTML(html.text)
     content = selector.xpath("//html")
     for each in content:
         title = each.xpath('//div[@class="v-title"]/h1/@title')
+        print('title', title)
         if title:
             cid_html_1 = each.xpath('//div[@class="scontent"]/iframe/@src')
             cid_html_2 = each.xpath('//div[@class="scontent"]/script/text()')
@@ -53,14 +57,14 @@ def get_comments(av):
                         comments_time_list.append(comment_time_int)
                         # print('add time', comment_time_int)
                 print('done fetching comments, total comments:', cnt)
-                return comments_time_list, int(max_second)
+                return comments_time_list, int(max_second), title
             else:
                 print('error')
         else:
             print('video not found!')
 
 
-def process_time(comments_time_list, max_second):
+def process_time(comments_time_list, max_second, title):
     max_minute = int(decimal.Decimal(max_second / 60).quantize(decimal.Decimal('1'), rounding=decimal.ROUND_CEILING))
     print('max_minute', max_minute)
     print('max_second', max_second)
@@ -68,7 +72,7 @@ def process_time(comments_time_list, max_second):
     # print(freqs)
     f = open('flare2.json', 'w')
     flare = dict()
-    flare['name'] = 'flare'
+    flare['name'] = title
     children_level_one = []
     for i in range(max_minute):
         child_one = dict()
@@ -78,11 +82,10 @@ def process_time(comments_time_list, max_second):
         children_level_one.append(child_one)
         start_second = i * 60
         for j in range(start_second, start_second + 60):
-            if freqs.get(j):
-                child_two = dict()
-                child_two['name'] = '{}:{}'.format(str(i), j - start_second)
-                child_two['size'] = freqs.get(j)
-                children_level_two.append(child_two)
+            child_two = dict()
+            child_two['name'] = '{}:{}'.format(str(i), j - start_second)
+            child_two['size'] = freqs.get(j, 0)
+            children_level_two.append(child_two)
     flare['children'] = children_level_one
     json_dump = json.dumps(flare, indent=4)
     # print(json_dump)
@@ -92,7 +95,7 @@ def process_time(comments_time_list, max_second):
 if __name__ == '__main__':
     # av = raw_input('input av:')
     av = '3133255'
-    comments_time_list, max_second = get_comments(av)
+    comments_time_list, max_second, title = get_comments(av, 10)
     # comments_time_list = list(range(2800))
     # max_second = len(comments_time_list)
-    process_time(comments_time_list, max_second)
+    process_time(comments_time_list, max_second, title)
